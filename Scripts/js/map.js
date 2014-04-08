3dbf2704c7c7d057ecbb85c30abef0fe2bc9773b
NickyH <nicky.hughes@tapestry.net>
2014-04-08 11:25:58 +1000

    added layers and search by category qtip

--- Scripts/js/map.js
+++ Scripts/js/map.js
@@ -1,5 +1,145 @@
+var owlLayersHtml;
 
 //dom ready functions
 $(function(){
-
+  create_layers_carousel();
+  $("#insert-left").on('click', '#search-by-category-button', search_by_category_qtip);
+  $("#insert-left").on('click', '#layers-button', layers_qtip);
 });
+
+function layers_qtip() {
+  $(this).qtip({
+      content: {
+        text: $('.layers-owl-wrapper'),
+        button: 'Close'
+      },
+      show: {
+          modal: {
+              on: true,
+              solo: true
+          },
+
+          ready: true,
+          event: 'click',
+          effect: function (offset) {
+              $(this).slideDown(300);
+          }
+      },
+      style: {
+          classes: 'qtip-layers-panel qtip-rounded qtip-shadow qtip-light',
+          tip: {
+            corner: 'center left',
+            width: 50,
+            height: 30
+        }
+      },
+      hide: {
+          event: 'click',
+          effect: function () {
+              $(this).slideUp(300);
+          }
+      },
+      overwrite: false,
+      position: {
+          my: 'center left',
+          at: 'center right',
+          target: $(this),
+          adjust: {
+            scroll: true // Can be ommited (e.g. default behaviour)
+        }
+      }
+  });
+}
+
+function search_by_category_qtip() {
+  $(this).qtip({
+      content: {
+        text: $('#search-by-category'),
+        button: 'Close'
+      },
+      show: {
+          modal: {
+              on: true,
+              solo: true
+          },
+
+          ready: true,
+          event: 'click',
+          effect: function (offset) {
+              $(this).slideDown(300);
+          }
+      },
+      style: {
+          classes: 'qtip-search-category qtip-rounded qtip-shadow qtip-light',
+          tip: {
+            corner: 'center left',
+            width: 50,
+            height: 30,
+            target: $('#search-by-category-button')
+        }
+      },
+      hide: {
+          event: 'click',
+          effect: function () {
+              $(this).slideUp(300);
+          }
+      },
+      overwrite: false,
+      position: {
+          my: 'center left',
+          at: 'center right',
+          target: $('#search-by-category-button')
+      }
+  });
+  $('#address-search-panel').removeClass('invisible');
+}
+
+function create_layers_carousel() {
+  owlLayersHtml = $("#owl-example").owlCarousel({
+
+    // Most important owl features
+    items : 3,
+    itemsDesktop : [1199,3],
+    itemsDesktopSmall : [980,3],
+    itemsTablet: [800,3],
+    itemsTabletSmall: [400,2],
+    itemsMobile : [479,1],
+    singleItem : false,
+    itemsScaleUp : false,
+
+    //Basic Speeds
+    slideSpeed : 200,
+    paginationSpeed : 800,
+    rewindSpeed : 1000,
+
+    //Autoplay
+    autoPlay : false,
+    stopOnHover : false,
+
+    // Navigation
+    navigation : true,
+    navigationText : false,
+    rewindNav : true,
+    scrollPerPage : false,
+
+    //Pagination
+    pagination : true,
+    paginationNumbers: true,
+
+    // Responsive
+    responsive: true,
+    responsiveRefreshRate : 100,
+    responsiveBaseWidth: window,
+
+    // CSS Styles
+    baseClass : "owl-carousel",
+
+    //Auto height
+    autoHeight : false,
+
+    //Transitions
+    transitionStyle : false,
+    })
+  $('.owl-prev').addClass('arrow-left');
+  $('.owl-next').addClass('arrow-right');
+}
